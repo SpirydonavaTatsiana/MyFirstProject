7e4ee074e47c8a1a245f38a0b5af831274213138
Spirydonava Tatsiana <spirydonavatatsiana@gmail.com>
2024-07-06 14:32:51 +0300

    task 6

--- src/main/java/ChangingArray.java
+++ src/main/java/ChangingArray.java
@@ -1,4 +1,4 @@
-public class main {
+public class ChangingArray {
     public static void main(String[] args) {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
